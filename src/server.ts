e8e0906e57505160100b12ba2db76f6dd938d424
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-06-21 12:35:12 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import "dotenv/config";
+import { prisma } from "./lib/prisma";
+const port = process.env.PORT || 5000;
+
+const main = async () => {
+  try {
+    await prisma.$connect();
+    console.log("connect db successfully");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+};
+
+main();
