71614972d7a83e95c59eddbce3f825e29015597c
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-06-26 06:20:14 +0600

    Add JWT utility functions and update authentication service for token generation

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,31 +1,64 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
-import { ILoginUser } from "./auth.interface"
+import { ILoginUser } from "./auth.interface";
 
-const logingUser= async(payload:ILoginUser)=>{
+import jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
-    const {email,password}=payload;
-    // const user=await prisma.user.findUnique({
-    //     where:{email}
-    // })
-    // if(!user){
-    //     throw new Error('user not found')
-    // }
+const logingUser = async (payload: ILoginUser) => {
+  const { email, password } = payload;
+  // const user=await prisma.user.findUnique({
+  //     where:{email}
+  // })
+  // if(!user){
+  //     throw new Error('user not found')
+  // }
 
-    const user=await prisma.user.findUniqueOrThrow({
-        where:{email}
-    })
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { email },
+  });
 
-    const isPasswordMatched=await bcrypt.compare(password,user.password)
+  const isPasswordMatched = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordMatched){
-        throw new Error("Password is incorrect")
-    }
+  if (!isPasswordMatched) {
+    throw new Error("Password is incorrect");
+  }
 
-    return user;
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
 
-}
+  // const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret, {
+  //   expiresIn: config.jwt_access_exprire_in,
+  // } as SignOptions);
 
-export const authService={
-    logingUser
-}
+  const accessToken=jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_exprire_in as SignOptions
+  )
+
+  // const refreshToken = jwt.sign(jwtPayload, config.jwt_refresh_exprire_in, {
+  //   expiresIn: config.jwt_refresh_exprire_in,
+  // } as SignOptions);
+
+  const refreshToken=jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_exprire_in as SignOptions
+  )
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+  // return user;
+};
+
+export const authService = {
+  logingUser,
+};
